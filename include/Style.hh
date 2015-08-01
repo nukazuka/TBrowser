a0069e0733ef1a3884a5a53f674e24a167dbf274
Genki Nukazuka <nukadukagenki@gmail.com>
2015-08-01 14:15:19 +0200

    previous change did not effected.why?

---
+++ include/Style.hh
@@ -0,0 +1,13 @@
+#ifndef style_hh
+#define style_hh
+
+#ifndef ROOT_TStyle
+#include "TStyle.h"
+#endif
+
+#include "TROOT.h"
+#include "TPad.h"
+TStyle* Style();
+
+//void Style();
+#endif // style_hh
